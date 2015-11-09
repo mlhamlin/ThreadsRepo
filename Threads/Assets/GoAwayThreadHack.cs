5897d4716f8045de5b5731647ed1a12c184da430
Martha Hamlin <mlhamlin92@gmail.com>
2015-11-08 22:20:08 -0500

    Weeeeee code debt, temporary hack to hide Sonkeena thread when she decides no relationship

---
+++ Threads/Assets/GoAwayThreadHack.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoAwayThreadHack : MonoBehaviour {
+
+    public Thread thread;
+    public string flag;
+	
+	// Update is called once per frame
+	void Update () {
+        thread.setVisibility(!GameStateDictionary.CheckFlag(flag));
+	}
+}
