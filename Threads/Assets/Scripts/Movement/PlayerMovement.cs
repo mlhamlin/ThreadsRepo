07a08535a61619521da2dbcec8087c8d6ff5e63b
Martha Hamlin <mlhamlin92@gmail.com>
2015-12-09 15:11:00 -0500

    updating backgrounds and getting ready for indoor audio tracks

--- Threads/Assets/Scripts/Movement/PlayerMovement.cs
+++ Threads/Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed = 1.0f;
+    Rigidbody bod; 
 
     // Use this for initialization
     void Start () {
