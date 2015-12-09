8bf8dc2592a012e3ae77a2f780b5b73beb91ee23
Martha Hamlin <mlhamlin92@gmail.com>
2015-12-09 16:00:53 -0500

    adding swapping direction when moving

--- Threads/Assets/Scripts/Movement/PlayerMovement.cs
+++ Threads/Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed = 1.0f;
-    Rigidbody bod; 
+    public Sprite FacingLeft;
+    public Sprite FacingRight;
+    public SpriteRenderer Renderer;
 
     // Use this for initialization
     void Start () {
@@ -17,6 +19,15 @@ public class PlayerMovement : MonoBehaviour {
         if(!DialogController.Instance.Active)
         {
             var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+            if (move.x >= 0f)
+            {
+                Renderer.sprite = FacingRight;
+            } else
+            {
+                Renderer.sprite = FacingLeft;
+            }
+
             transform.position += move * speed * Time.deltaTime;
         }
     }
