989c08b3764f59ee5471a656501641fad3d17f24
Martha Hamlin <mlhamlin92@gmail.com>
2016-01-24 17:21:41 -0500

    Adding exit and restart buttons and reorganizing a bit

---
+++ ThreadsMobile/Assets/Scripts/SceneNavigation/SceneControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneControl: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Restart()
+    {
+        //TODO: Maybe once we have a puzzle save file we can replace this with something smarter?
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitApp()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #elif UNITY_WEBPLAYER
+        Application.OpenURL(webplayerQuitURL);
+    #else
+        Application.Quit();
+    #endif
+    }
+}
