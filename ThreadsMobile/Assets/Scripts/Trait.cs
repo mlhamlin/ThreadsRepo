d3f86e84ded5a391820748cda6234954c643767a
Dylan Mayerchak <dylanmayerchak@gmail.com>
2016-01-28 12:15:45 -0500

    * Changed keys for Traits from int to string * Separated Traits into their own class

---
+++ ThreadsMobile/Assets/Scripts/Trait.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trait {
+
+	const string ICON_PATH = "Icons/";
+
+	string traitName;
+	string traitDescription;
+	string icon;
+
+	public string TraitName {
+		get{ return traitName; }
+	}
+
+	public string TraitDescription {
+		get{ return traitDescription; }
+	}
+
+	public Sprite Icon {
+		get{ return Resources.Load<Sprite> (ICON_PATH + icon); }
+	}
+
+	public Trait(){
+	}
+
+	public Trait(string traitName, string traitDescription, string icon) : this()
+	{
+		this.traitName = traitName;
+		this.traitDescription = traitDescription;
+		this.icon = icon;
+	}
+
+	public static Trait ErrorTrait(){
+		return new Trait("error", "error", "error");
+	}
+}
--- ThreadsMobile/Assets/Scripts/TraitManager.cs
+++ ThreadsMobile/Assets/Scripts/TraitManager.cs
@@ -5,11 +5,26 @@ using Newtonsoft.Json;
 
 public class TraitManager : UnitySingletonPersistent<TraitManager> {
 
+	/*
     public struct Trait
     {
+		const string ICON_PATH = "Icons/";
+
         string traitName;
         string traitDescription;
         string icon;
+
+		public string TraitName {
+			get{ return traitName; }
+		}
+
+		public string TraitDescription {
+			get{ return traitDescription; }
+		}
+
+		public Sprite Icon {
+			get{ return Resources.Load<Sprite> (ICON_PATH + icon); }
+		}
 
         public Trait(string traitName, string traitDescription, string icon) : this()
         {
@@ -21,31 +36,31 @@ public class TraitManager : UnitySingletonPersistent<TraitManager> {
 		public static Trait ErrorTrait(){
 			return new Trait("error", "error", "error");
 		}
-    }
+    }*/
 
-    public Dictionary<int, Trait> traits;
+    public Dictionary<string, Trait> traits;
 	
 	JsonSerializerSettings settings;
 	
 	public void Start()
 	{
-		traits = new Dictionary<int, Trait>();
+		traits = new Dictionary<string, Trait>();
 		
 		settings = new JsonSerializerSettings();
 		settings.TypeNameHandling = TypeNameHandling.Objects;
 
 		TextAsset traitsFile = Resources.Load<TextAsset>("Characters/Traits");
 		
-		Instance.traits = JsonConvert.DeserializeObject<Dictionary<int, Trait>>(traitsFile.text, Instance.settings);
+		Instance.traits = JsonConvert.DeserializeObject<Dictionary<string, Trait>>(traitsFile.text, Instance.settings);
 	}
 
-	public static Trait GetTrait(int id){
+	public static Trait GetTrait(string id){
 		Trait value;
 
 		if(Instance.traits.TryGetValue(id, out value)){
 			return value;
 		}else{
-			Debug.LogError("Trait #" + id + " not found");
+			Debug.LogError("Trait " + id + " not found");
 			return Trait.ErrorTrait();
 		}
 	}
