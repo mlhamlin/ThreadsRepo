845a3c40cd6150fb69548e678723023bc42f9f28
Elizabeth Renn <elizabethrenn@Elizabeths-MacBook-Pro.local>
2016-02-01 00:15:48 -0500

    Figuring out tool tips kinda

---
+++ ThreadsMobile/Assets/Scripts/Traits/TraitData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using TouchScript.Hit;
+using System;
+
+[RequireComponent(typeof(PressGesture), typeof(ReleaseGesture))]
+public class TraitData : MonoBehaviour {
+
+
+}
+
