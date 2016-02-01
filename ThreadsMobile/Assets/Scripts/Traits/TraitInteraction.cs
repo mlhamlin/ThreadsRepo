e643ed3b3eeee8da744ea09cbec8296b472e69b5
Elizabeth Renn <elizabethrenn@Elizabeths-MacBook-Pro.local>
2016-02-01 12:51:33 -0500

    Working on tool tips

---
+++ ThreadsMobile/Assets/Scripts/Traits/TraitInteraction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using System;
+
+[RequireComponent(typeof(TapGesture))]
+public class TraitInteraction : MonoBehaviour {
+
+	private TapGesture tapBack;
+
+	private void OnEnable()
+	{
+		GetComponent<TapGesture>().Tapped += tappedHandler;
+	}
+
+	private void OnDisable()
+	{
+		GetComponent<TapGesture>().Tapped -= tappedHandler;
+	}
+
+	private void tappedHandler(object sender, EventArgs e)
+	{
+		
+	}
+}
+
