506eb9cc2d6842b50e208356ef186687c499bb2e
Martha Hamlin <mlhamlin92@gmail.com>
2016-02-15 19:56:46 -0500

    Starting to fix issue with clicking through debug panel making things difficult on android

---
+++ ThreadsMobile/Assets/BlockOnDebug.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockOnDebug : MonoBehaviour {
+
+    public GameObject Blocker;
+    public Reporter rep;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Blocker.SetActive(rep.show);
+	}
+}
--- ThreadsMobile/Assets/Scripts/SceneNavigation/SceneControl.cs
+++ ThreadsMobile/Assets/Scripts/SceneNavigation/SceneControl.cs
@@ -5,24 +5,27 @@ using UnityEngine.UI;
 
 public class SceneControl: MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public Reporter rep;
+
+    public void Start()
+    {
+        rep = FindObjectOfType<Reporter>();
+    }
 
     public void Restart()
     {
+        if(rep.show)
+            return;
         //TODO: Maybe once we have a puzzle save file we can replace this with something smarter?
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void ExitApp()
     {
+        if(rep.show)
+            return;
+
     #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
     #elif UNITY_WEBPLAYER
@@ -34,7 +37,10 @@ public class SceneControl: MonoBehaviour {
 
 	public void Submit()
 	{
-		int totalScore = 0;
+        if(rep.show)
+            return;
+
+        int totalScore = 0;
 		GameObject[] characters = GameObject.FindGameObjectsWithTag ("Character");
 
 		for (int i = 0; i < characters.Length; i++) {
