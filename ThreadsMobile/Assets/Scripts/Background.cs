8bfb172b52f96473c04f3e7a7ca468a7c9694184
Martha Hamlin <mlhamlin92@gmail.com>
2016-02-22 13:00:52 -0500

    starting to implement panning

--- ThreadsMobile/Assets/Scripts/Background.cs
+++ ThreadsMobile/Assets/Scripts/Background.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using TouchScript.Gestures;
+using TouchScript.Behaviors;
 
 public class Background : UnitySingleton<Background> {
 
     private TapGesture tapBack;
 
-    // Use this for initialization
-    void Start () {
+    private void Start()
+    {
         tapBack = gameObject.GetComponent<TapGesture>();
     }
 }
